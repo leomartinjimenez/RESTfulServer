aee5dbf488bbb8b3f04d0c72c23718994d1c7fa0
Leo Martin <lmartinjimen@csc.com>
2019-03-18 12:51:13 +0100

    Adding a library NPM body-parser-xml
    It's used for parser incoming XML body requests.

--- server/server.js
+++ server/server.js
@@ -6,12 +6,66 @@ const mongoose = require('mongoose')
 
 const app = express()
 const bodyParser = require('body-parser')
+require('body-parser-xml-json')(bodyParser);
+
+// NPM FHIR library
+//-------------------------------------------
+// https://www.npmjs.com/package/fhir
+//-------------------------------------------
+const ParseConformance = require('fhir').ParseConformance;
+const FhirVersions = require('fhir').Versions;
+const Fhir = require('fhir').Fhir;
+//-------------------------------------------
+
+/* let resource = {
+    "resourceType": "Patient",
+    "id": "1001",
+    "identifier": [
+            {
+                    "type": {
+                            "text": "NHC"
+                    },
+                    "system": "xHis",
+                    "value": "HC1001"
+            }
+    ],
+    "name": [
+            {
+                    "text": "ADLER, REBECCA JANE",
+                    "family": "ADLER",
+                    "given": [
+                            "REBECCA JANE"
+                    ]
+            }
+    ],
+    "gender": "male",
+    "birthDate": "1977-08-21"
+}
+
+let fhir = new Fhir()
+let xml = fhir.objToXml(resource);
+let json = fhir.xmlToJson(xml);
+let obj = fhir.xmlToObj(xml);
+let results = fhir.validate(xml, { errorOnUnexpected: true });
+results = fhir.validate(obj, {});
+console.log(xml)
+console.log(json)
+console.log(obj)
+console.log(results) */
+
+
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+
+// parse application/xml
+app.use(bodyParser.xml())
+ 
 
 // import usuario.js
 app.use(require('./routes/usuario'))
